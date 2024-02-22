60bb2c280f1ba322c2963662fd2496926c6fec10
Erik Rasmussen <erik.rasmussen@unmango.dev>
2024-02-21 22:24:04 -0600

    More tests!

--- api/v1alpha1/cloudflareddeployment_types_test.go
+++ api/v1alpha1/cloudflareddeployment_types_test.go
@@ -3,6 +3,8 @@ package v1alpha1
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ = Describe("CloudflaredDeployment", func() {
@@ -25,6 +27,18 @@ var _ = Describe("CloudflaredDeployment", func() {
 				Expect(actual.Name).To(Equal(deployment.Name))
 				Expect(actual.Namespace).To(Equal(deployment.Namespace))
 			})
+
+			It("should configure the DaemonSet", func() {
+				By("Calling ToDaemonSet")
+				actual := deployment.ToDaemonSet("test")
+
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Spec.Selector.MatchLabels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Labels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers).To(HaveLen(1))
+				Expect(actual.Spec.Template.Spec.Containers[0].Name).To(Equal("cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers[0].Image).To(Equal("test"))
+			})
 		})
 
 		Context("and kind is DaemonSet", func() {
@@ -38,6 +52,58 @@ var _ = Describe("CloudflaredDeployment", func() {
 				actual := deployment.ToDaemonSet("test")
 
 				Expect(actual).NotTo(BeNil())
+			})
+
+			It("should use the deployment's name and namespace", func() {
+				By("Calling ToDaemonSet")
+				actual := deployment.ToDaemonSet("test")
+
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Name).To(Equal(deployment.Name))
+				Expect(actual.Namespace).To(Equal(deployment.Namespace))
+			})
+
+			It("should configure the DaemonSet", func() {
+				By("Calling ToDaemonSet")
+				actual := deployment.ToDaemonSet("test")
+
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Spec.Selector.MatchLabels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Labels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers).To(HaveLen(1))
+				Expect(actual.Spec.Template.Spec.Containers[0].Name).To(Equal("cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers[0].Image).To(Equal("test"))
+			})
+		})
+
+		Context("and a custom template is configured", func() {
+			BeforeEach(func() {
+				By("Configuring the pod template")
+				deployment.Spec.Template = &v1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{
+							"app": "cloudflared",
+						},
+					},
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{{
+							Name:  "cloudflared",
+							Image: "testing",
+						}},
+					},
+				}
+			})
+
+			It("should honor the configured template", func() {
+				By("Calling ToDaemonSet")
+				actual := deployment.ToDaemonSet("test")
+
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Spec.Selector.MatchLabels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Labels).To(HaveKeyWithValue("app", "cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers).To(HaveLen(1))
+				Expect(actual.Spec.Template.Spec.Containers[0].Name).To(Equal("cloudflared"))
+				Expect(actual.Spec.Template.Spec.Containers[0].Image).To(Equal("testing"))
 			})
 		})
 	})
